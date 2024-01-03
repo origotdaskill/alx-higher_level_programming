0c9030896edf3e70841de66c2cb523b262d0e7f8
origotdaskill <mr.walid.ori@gmail.com>
2024-01-03 23:43:57 +0100

    ori commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -12,7 +12,3 @@ def uppercase(str):
         else:
             print("{:c}".format(ord(char)), end="")
     print()
-
-if __name__ == "__main__":
-    uppercase("best")
-    uppercase("Best School 98 Battery street")
