5bbf12f658a2a871952aa6b46ba5a0f2209d96fb
origotdaskill <mr.walid.ori@gmail.com>
2024-01-03 23:56:41 +0100

    ori commit

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """
+    Computes a to the power of b and returns the value.
+    Args:
+        a: The base.
+        b: The exponent.
+    Returns:
+        The result of a raised to the power of b.
+    """
+    return a ** b
