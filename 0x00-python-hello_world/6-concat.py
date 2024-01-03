d8736c7b8e245b529b49f96b3f70b407abbca71b
origotdaskill <mr.walid.ori@gmail.com>
2024-01-03 20:36:43 +0100

    ori commit

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "holberton"
-str2 = "School!"
-str3 = "welcome to"
-print(f"{str3} {str1} {str2}")
+str1 = "Holberton"
+str2 = "School"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"Welcome to {str1} {str2}!")
