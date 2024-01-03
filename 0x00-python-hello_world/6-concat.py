add129f61d49a801940155cdbbb7bd7d1050a0fb
origotdaskill <mr.walid.ori@gmail.com>
2024-01-03 20:33:35 +0100

    ori commit

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = "Holberton School!"
-print(f"{str1} {str2}")
+str1 = "holberton"
+str2 = "School!"
+str3 = "welcome to"
+print(f"{str3} {str1} {str2}")
