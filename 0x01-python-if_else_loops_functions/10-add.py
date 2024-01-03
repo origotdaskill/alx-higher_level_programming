cb06657251f06973229008f37e160fb32a6835e3
origotdaskill <mr.walid.ori@gmail.com>
2024-01-03 23:53:35 +0100

    ori commit

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """
+    Adds two integers and returns the result.
+    Args:
+        a: The first integer.
+        b: The second integer.
+    Returns:
+        The sum of a and b.
+    """
+    return a + b
