8e5cb7ec3d2e8f907a3bb586f102c0aa4fbca8be
origotdaskill <mr.walid.ori@gmail.com>
2024-01-07 17:28:06 +0100

    ori commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-
 a = 1
 b = 2
-add_0 = __import__('0-add').add
-print("{} + {} = {}".format(a, b, add_0(a, b)))
+from add_0 import add
+print("{} + {} = {}".format(a, b, add(a, b)))
+
