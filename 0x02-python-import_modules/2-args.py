24e29911e51b0694ad8ebbdaf4822703a5953207
origotdaskill <mr.walid.ori@gmail.com>
2024-01-07 18:02:31 +0100

    ori commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     num_args = len(argv) - 1  # subtract 1 to exclude the script name
     args = argv[1:] if num_args > 0 else []
 
-    print("{} argument{}{}".format(num_args, 's' if num_args != 1 else '', ':' if num_args > 0 else '.'))
+    print("{} argument{}{}".format(
+        num_args, 's' if num_args != 1 else '', ':' if num_args > 0 else '.'))
 
     for i, arg in enumerate(args, start=1):
         print("{}: {}".format(i, arg))
