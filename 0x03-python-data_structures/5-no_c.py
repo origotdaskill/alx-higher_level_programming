ffe19987804bbf4d42908fe49318ec9e946b6fda
origotdaskill <mr.walid.ori@gmail.com>
2024-01-07 22:56:09 +0100

    ori commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
 
 def no_c(my_string):
     new_string = ""
     for char in my_string:
-        if char != 'c' and char != 'C':
+        if char.lower() != 'c':
             new_string += char
     return new_string
-
-if __name__ == "__main__":
-    print(no_c("Best School"))
-    print(no_c("Chicago"))
-    print(no_c("C is fun!"))
