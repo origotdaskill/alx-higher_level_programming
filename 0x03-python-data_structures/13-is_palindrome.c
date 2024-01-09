56348dd0202e46f5c0e8c8ac38b8a8ec2dda6f95
origotdaskills <mr.walid.ori@gmail.com>
2024-01-09 10:49:50 +0100

    ori commit

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,41 @@
 #include "lists.h"
 
 /**
- * reverse_list - reverses a linked list
- * @head: pointer to the head of the list
- * Return: pointer to the new head of the reversed list
- */
-listint_t *reverse_list(listint_t *head)
-{
-    listint_t *prev = NULL, *current = head, *next = NULL;
-
-    while (current != NULL)
-    {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
-
-    return prev;
-}
-/**
  * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to the head of the list
+ * @head: pointer to pointer to the head of the list
  * Return: 1 if palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-    if (*head == NULL || (*head)->next == NULL)
-        return 1;
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev = NULL;
+    listint_t *temp;
 
-    listint_t *slow = *head, *fast = *head;
-    listint_t *prev_slow = NULL, *mid = NULL;
-    listint_t *second_half_reversed = NULL, *temp = NULL;
+    if (*head == NULL || (*head)->next == NULL)
+        return (1);
 
     while (fast != NULL && fast->next != NULL)
     {
-        prev_slow = slow;
-        slow = slow->next;
         fast = fast->next->next;
+
+        temp = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = temp;
     }
 
-    if (fast != NULL)  // Odd number of nodes, skip the middle one
+    if (fast != NULL)
+        slow = slow->next;
+
+    while (prev != NULL && slow != NULL)
     {
-        mid = slow;
+        if (prev->n != slow->n)
+            return (0);
+        prev = prev->next;
         slow = slow->next;
     }
 
-    // Reverse the second half
-    second_half_reversed = reverse_list(slow);
-
-    // Compare the first half with the reversed second half
-    temp = *head;
-    while (second_half_reversed != NULL)
-    {
-        if (temp->n != second_half_reversed->n)
-        {
-            // Restore the original list and return false
-            prev_slow->next = mid;
-            mid->next = reverse_list(second_half_reversed);
-            return 0;
-        }
-        temp = temp->next;
-        second_half_reversed = second_half_reversed->next;
-    }
-
-    // Restore the original list
-    prev_slow->next = mid;
-    mid->next = reverse_list(second_half_reversed);
-
-    return 1;
+    return (1);
 }
+
