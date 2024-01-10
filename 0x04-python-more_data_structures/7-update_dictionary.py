22855b7e467373faaf25fa0aa34d2be419c05b51
origotdaskills <mr.walid.ori@gmail.com>
2024-01-10 11:58:44 +0100

    ori commit

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Update dictionary"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value in a dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary
