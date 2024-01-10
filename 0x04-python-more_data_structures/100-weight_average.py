ee2634e81a4bafadd7bf3a28999f752bbb0b1415
origotdaskills <mr.walid.ori@gmail.com>
2024-01-10 12:25:41 +0100

    ori commit

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Weighted Average"""
+
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    total_score = 0
+    total_weight = 0
+
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
+
+    return total_score / total_weight
