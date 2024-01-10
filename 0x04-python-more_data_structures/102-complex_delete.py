089f466b065944f9c59c7ea901a99a4e3f99eb72
origotdaskills <mr.walid.ori@gmail.com>
2024-01-10 12:29:45 +0100

    ori commit

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Delete by value"""
+
+
+def complex_delete(a_dictionary, value):
+    """Deletes keys with a specific value in a dictionary"""
+    keys_to_delete = [key for key, val in a_dictionary.items() if val == value]
+
+    for key in keys_to_delete:
+        del a_dictionary[key]
+
+    return a_dictionary
