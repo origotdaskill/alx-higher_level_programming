6a72f4fa6fe2d7fadb9276a3a9344bff55e6ae98
origotdaskills <mr.walid.ori@gmail.com>
2024-01-10 11:32:44 +0100

    ori commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,4 +6,3 @@ def square_matrix_simple(matrix=[]):
     """Compute the square value of all integers of a matrix."""
     new_matrix = [list(map(lambda x: x ** 2, row)) for row in matrix]
     return new_matrix
-
