d516f63840b0e233be5111a3166d5659d631972b
origotdaskill <mr.walid.ori@gmail.com>
2024-01-23 21:50:40 +0100

    ori commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Class Square with a private instance attribute: size"""
+
+    def __init__(self, size):
+        """Initializes a new instance of the Square class."""
+        self.__size = size
