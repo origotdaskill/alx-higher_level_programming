19ad24d5026c4e5c7f72b2d1fcfda652a82a3100
origotdaskill <mr.walid.ori@gmail.com>
2024-01-23 22:07:20 +0100

    ori commit

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Defines a square with size and position."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes a new instance of the Square class."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Getter method to retrieve the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method to set the size with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        """Getter method to retrieve the position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Setter method to set the position with validation."""
+        if not isinstance(value, tuple) or len(value) != 2 or \
+                not all(isinstance(i, int) for i in value) or \
+                any(i < 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        """Computes the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with the character #."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        """String representation of the square."""
+        return self.my_print()
