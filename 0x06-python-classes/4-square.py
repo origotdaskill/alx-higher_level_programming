f2e84546cfde0f4e8c37e5a14ff371f5c1301188
origotdaskill <mr.walid.ori@gmail.com>
2024-01-23 21:57:50 +0100

    ori commit

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Class Square with a private instance attribute: size"""
+
+    def __init__(self, size=0):
+        """Initializes a new instance of the Square class."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter method to retrieve the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method to set the size with validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculates and returns the area of the square."""
+        return self.__size ** 2
