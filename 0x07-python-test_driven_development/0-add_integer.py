76e574e06aa531eeeb98355b50148d7981b140e2
origotdaskill <mr.walid.ori@gmail.com>
2024-01-27 15:03:12 +0100

    ori commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+    - a (int or float): The first number.
+    - b (int or float, default: 98): The second number.
+
+    Returns:
+    int: The addition of a and b.
+
+    Raises:
+    TypeError: If a or b is not an integer or float.
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    return int(a) + int(b)
+
+
---
+++ 0x07-python-test_driven_development/0-main.py
@@ -0,0 +1,6 @@
+origotdaskills@origotdaskills:~/alx-higher_level_programming/0x07-python-test_driven_development/tests$ ./0-main.py
+Traceback (most recent call last):
+  File "/home/origotdaskills/alx-higher_level_programming/0x07-python-test_driven_development/tests/./0-main.py", line 2, in <module>
+    add_integer = __import__('0-add_integer').add_integer
+ModuleNotFoundError: No module named '0-add_integer'
+origotdaskills@origotdaskills:~/alx-higher_level_programming/0x07-python-test_driven_development/tests$
