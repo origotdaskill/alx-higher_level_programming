53ce85dee6a08789b6552c3a15860e81c7c305f4
origotdaskill <mr.walid.ori@gmail.com>
2024-02-06 00:12:16 +0100

    ori commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module with BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Raises an exception with a message"""
+        raise Exception("area() is not implemented")
