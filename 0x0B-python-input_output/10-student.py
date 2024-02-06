6f696f688402e14cd648c8e46cb751443d235216
origotdaskill <mr.walid.ori@gmail.com>
2024-02-07 00:58:00 +0100

    ori commit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module to define a Student class."""
+
+class Student:
+    """Class to represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list): List of attribute names to retrieve (default is None).
+
+        Returns:
+            dict: A dictionary representation of the student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
