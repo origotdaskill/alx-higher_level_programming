eb488a6717cedd69265317da59975ace2ea5f261
origotdaskill <mr.walid.ori@gmail.com>
2024-02-06 23:03:42 +0100

    ori commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module 0-read_file"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+this an other readme file for 0x0B-python-input_output repo
