96d869c0c39bb7311f7649cd6e55b87a945fd10a
origotdaskill <mr.walid.ori@gmail.com>
2024-02-06 23:23:02 +0100

    ori commit

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written."""
+    """Writes to file and returns number of characters written."""
     with open(filename, 'w', encoding='utf-8') as file:
         return file.write(text)
