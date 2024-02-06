b8618d27e9e1e563949fb38fa207042e6bc96fba
origotdaskill <mr.walid.ori@gmail.com>
2024-02-06 23:31:50 +0100

    ori commit

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file and returns the number of characters added."""
+    with open(filename, 'a', encoding='utf-8') as file:
+        file.write(text)
+    return len(text)
