46a6d9332ff071eed9ef7552e2901238303a6c4a
origotdaskill <mr.walid.ori@gmail.com>
2024-02-07 22:59:53 +0100

    ori commit

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Module to parse log lines and compute statistics."""
+
 import sys
+
 
 def print_stats(total_size, status_codes):
     """
@@ -7,13 +10,14 @@ def print_stats(total_size, status_codes):
 
     Args:
         total_size (int): Total file size.
-        status_codes (dict): Dictionary containing the count of each status code.
+        status_codes (dict): Dictionary containing the count of each status.
     """
     print("File size: {}".format(total_size))
     sorted_status_codes = sorted(status_codes.items())
     for code, count in sorted_status_codes:
         if count > 0:
             print("{}: {}".format(code, count))
+
 
 def parse_line(line):
     """
@@ -30,9 +34,20 @@ def parse_line(line):
     file_size = int(parts[-1])
     return status_code, file_size
 
+
 def main():
+    """Main function to read log lines from stdin and compute statistics."""
     total_size = 0
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    status_codes = {
+            200: 0,
+            301: 0,
+            400: 0,
+            401: 0,
+            403: 0,
+            404: 0,
+            405: 0,
+            500: 0
+            }
     try:
         for i, line in enumerate(sys.stdin, start=1):
             status_code, file_size = parse_line(line)
