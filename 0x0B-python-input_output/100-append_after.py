666201e7098ef365cde8811a4b257e688dfa9f20
origotdaskill <mr.walid.ori@gmail.com>
2024-02-07 01:23:47 +0100

    ori commit

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file after each line containing a specific string.
+
+    Args:
+        filename (str): The name of the file to append to.
+        search_string (str): The string to search for in each line.
+        new_string (str): The string to insert after each line containing the search string.
+    """
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
