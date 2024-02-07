9646e89877ad92eaa5da5ce149277a4490be8579
origotdaskill <mr.walid.ori@gmail.com>
2024-02-07 01:16:19 +0100

    ori commit

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    """Generate Pascal's Triangle up to the nth row."""
+    triangle = []
+
+    if n <= 0:
+        return triangle
+
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i >= 2:
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
+
+def print_triangle(triangle):
+    """Print the triangle."""
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
