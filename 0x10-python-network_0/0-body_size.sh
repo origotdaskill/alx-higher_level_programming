b8ed658fee98af4d1d3548dcd03d91f4de61a185
origotdaskill <mr.walid.ori@gmaill.com>
2024-04-26 18:26:09 +0100

    ori commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send request and store response body in a temporary file
-response=$(mktemp)
-curl -s -o "$response" "$1"
-
-# Calculate and display the size of the response body in bytes
-size=$(stat -c %s "$response")
-echo "$size"
-
-# Clean up temporary file
-rm "$response"
+# ends a request to that URL displays the size of the response body
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
